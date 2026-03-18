f23f4ed0a7e4139839e2023e9dd53323321c8cfe
Wen Hai Wang <qaz3440798292@gmail.com>
2026-03-18 14:03:13 +0800

    前端请求给认证接口的基本用户数据

---
+++ system-backend/src/main/java/cn/xumob/restaurant/dto/LoginDTO.java
@@ -0,0 +1,14 @@
+package cn.xumob.restaurant.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+
+    private String username;
+
+    private String password;
+
+    private String type;
+
+}
