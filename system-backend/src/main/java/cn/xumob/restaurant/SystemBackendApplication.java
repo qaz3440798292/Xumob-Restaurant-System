17e6a8e86606f641e6ac14ee146b6ac6c3f5bc23
Wen Hai Wang <qaz3440798292@gmail.com>
2026-03-18 08:33:34 +0800

    initial commit

---
+++ system-backend/src/main/java/cn/xumob/restaurant/SystemBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.xumob.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemBackendApplication.class, args);
+    }
+
+}
